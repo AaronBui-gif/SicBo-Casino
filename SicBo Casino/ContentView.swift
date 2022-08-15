92bf31b000b559883f73dedec19679565cb11dd3
AaronBui-gif <s3740934@rmit.edu.vn>
2022-08-15 21:48:10 +0700

    Initial Commit

---
+++ SicBo Casino/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SicBo Casino
+//
+//  Created by Huy Bui Thanh on 15/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SicBo Casino/SicBo_CasinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SicBo_CasinoApp.swift
+//  SicBo Casino
+//
+//  Created by Huy Bui Thanh on 15/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SicBo_CasinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
